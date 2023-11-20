e15a050ed77a985225dae692c57a1a5acaa11869
leonstefano03 <leonstefano03@gmail.com>
2023-11-20 18:09:47 -0300

    feat:section-after-header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,15 @@
 import Header from "@/components/Header";
 import Nav from "@/components/Nav";
-import Section from "@/components/Section";
+import SectionAfterHeadear from "@/components/SectionAfterHeader";
 
 export default function Home() {
   return (
     <main className="">
      <Nav/>
      <Header imgPeople={`/people.jpg`}/>
-     <Section/>
+    <SectionAfterHeadear/>
+    <Header imgPeople={`/people.jpg`}/>
+
     </main>
   )
 }
