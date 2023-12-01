e70c42dce79a75d2978ceea47feceb633a5000c2
leonstefano03 <leonstefano03@gmail.com>
2023-12-01 01:20:52 -0300

    feat:creating-view-SI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
-
 export default function Footer() {
-  const añoActual = new Date().getFullYear()
+  const añoActual = new Date().getFullYear();
   return (
     <footer className="flex w-full h-[6vh] bg-gray-900 justify-center items-center text-center">
-     <h1 className="text-gray-400">COMPANY-CO LS © {añoActual} | All rights reserved</h1>
+      <h1 className="text-gray-400">
+        COMPANY-CO LS © {añoActual} | All rights reserved
+      </h1>
     </footer>
   );
 }
